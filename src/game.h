7d258de5c0546665ef013683a4bc505457aef80e
KaiN <kain@piwnica.ws>
2018-09-04 15:26:05 +0200

    project stub

---
+++ src/game.h
@@ -0,0 +1,13 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+// Function headers from game.c go here
+// It's best to put here only those functions which are needed in other files.
+
+void gameGsCreate(void);
+
+void gameGsLoop(void);
+
+void gameGsDestroy(void);
+
+#endif // _GAME_H_
---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <ace/generic/main.h>
+#include <ace/managers/key.h>
+
+// Without it compiler will yell about undeclared gameGsCreate etc
+#include "game.h"
+
+void genericCreate(void) {
+  // Here goes your startup code
+  logWrite("Hello, Amiga!\n");
+  keyCreate(); // We'll use keyboard
+  // Initialize gamestate
+  gamePushState(gameGsCreate, gameGsLoop, gameGsDestroy);
+}
+
+void genericProcess(void) {
+  // Here goes code done each game frame
+  keyProcess();
+  gameProcess(); // Process current gamestate's loop
+}
+
+void genericDestroy(void) {
+  // Here goes your cleanup code
+  keyDestroy(); // We don't need it anymore
+  logWrite("Goodbye, Amiga!\n");
+}
