0176364bd0c74372fe43ff7876f6b53324eb971e
KaiN <kain@piwnica.ws>
2018-09-23 20:31:17 +0200

    health & fuel gauges: added labels to display and refresh stubs in code

--- src/hud.c
+++ src/hud.c
@@ -10,6 +10,10 @@
 typedef enum _tHudDraw {
 	HUD_PREPARE_DEPTH,
 	HUD_DRAW_DEPTH,
+	HUD_PREPARE_FUEL,
+	HUD_DRAW_FUEL,
+	HUD_PREPARE_HEALTH,
+	HUD_DRAW_HEALTH,
 	HUD_DRAW_END
 } tHudDraw;
 
@@ -39,6 +43,8 @@ void hudCreate(tView *pView) {
 	s_pFont = fontCreate("data/silkscreen5.fnt");
 	s_pLinebuffer = fontCreateTextBitMap(s_pHudBuffer->uBfrBounds.sUwCoord.uwX, 5);
 
+	fontDrawStr(s_pHudBuffer->pBack, s_pFont, 80, 0, "Fuel:", 15, FONT_LAZY);
+	fontDrawStr(s_pHudBuffer->pBack, s_pFont, 200, 0, "Hull:", 15, FONT_LAZY);
 	blitLine(s_pHudBuffer->pBack, 0, 15, s_pVpHud->uwWidth, 15, 1, 0xF0F0, 0);
 
 	s_uwOldDepth = 0xFFFF;
@@ -53,20 +59,53 @@ void hudSetDepth(UWORD uwDepth) {
 void hudUpdate(void) {
 	char szBfr[20];
 	static UBYTE isDepthPrepared = 0;
-	if(s_eDraw == HUD_PREPARE_DEPTH) {
-		if(s_uwDepth != s_uwOldDepth) {
-			sprintf(szBfr, "Depth: %5u", s_uwDepth);
-			fontFillTextBitMap(s_pFont, s_pLinebuffer, szBfr);
-			s_uwOldDepth = s_uwDepth;
-			isDepthPrepared = 1;
-		}
-	}
-	else if(s_eDraw == HUD_DRAW_DEPTH) {
-		if(isDepthPrepared) {
-			fontDrawTextBitMap(
-				s_pHudBuffer->pBack, s_pLinebuffer, 0, 0, 15, FONT_LAZY
-			);
-			isDepthPrepared = 0;
+	switch(s_eDraw) {
+		case HUD_PREPARE_DEPTH:{
+			if(s_uwDepth != s_uwOldDepth) {
+				sprintf(szBfr, "Depth: %5u", s_uwDepth);
+				fontFillTextBitMap(s_pFont, s_pLinebuffer, szBfr);
+				s_uwOldDepth = s_uwDepth;
+				isDepthPrepared = 1;
+			}
+			else {
+				// Skip drawing
+				++s_eDraw;
+			}
+		} break;
+		case HUD_DRAW_DEPTH: {
+			if(isDepthPrepared) {
+				fontDrawTextBitMap(
+					s_pHudBuffer->pBack, s_pLinebuffer, 0, 0, 15, FONT_LAZY
+				);
+				isDepthPrepared = 0;
+			}
+		} break;
+		case HUD_PREPARE_FUEL: {
+			if(0) {
+
+			}
+			else {
+				// Skip drawing
+				++s_eDraw;
+			}
+		} break;
+		case HUD_DRAW_FUEL: {
+
+		} break;
+		case HUD_PREPARE_HEALTH: {
+			if(0) {
+
+			}
+			else {
+				// Skip drawing
+				++s_eDraw;
+			}
+		} break;
+		case HUD_DRAW_HEALTH: {
+
+		} break;
+		default: {
+
 		}
 	}
 	++s_eDraw;
