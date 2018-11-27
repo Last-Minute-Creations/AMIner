bc86f5c1327025b9d26234959508aff82afb1a6b
KaiN <kain@piwnica.ws>
2018-11-27 20:44:20 +0100

    shop window displays something

--- src/vendor.c
+++ src/vendor.c
@@ -7,9 +7,104 @@
 #include <ace/managers/game.h>
 #include "window.h"
 #include "game.h"
+#include "tile.h"
 
 static UBYTE s_isShown;
+static tTextBitMap *s_pTextBitmap;
 tBitMap *s_pBuffer;
+
+typedef struct _tPlanMineral {
+	UBYTE ubMineralType;
+	UBYTE ubCount;
+} tPlanMineral;
+
+typedef struct _tPlan {
+	tPlanMineral pMinerals[4];
+	UBYTE ubMineralCount;
+	ULONG ulAlternativeSum;
+} tPlan;
+
+static const char *s_pMineralNames[] = {0,
+	[TILE_SILVER_1] = "Silver",
+	[TILE_GOLD_1] = "Gold",
+	[TILE_EMERALD_1] = "Emerald",
+	[TILE_RUBY_1] = "Ruby",
+	[TILE_MOONSTONE_1] = "Moonstone",
+	[TILE_COAL_1] = "Coal"
+};
+
+void vendorDrawPlan(void) {
+	tUwCoordYX sOrigin = windowGetOrigin();
+	char *szColNames[3] = {"Mineral", "Required", "Gathered"};
+	uint8_t pColOffs[3] = {16, 120, 200};
+	UBYTE ubRowOffsY = sOrigin.sUwCoord.uwY + 16;
+	for(UBYTE i = 0; i < 3; ++i) {
+		fontFillTextBitMap(g_pFont, s_pTextBitmap, szColNames[i]);
+		fontDrawTextBitMap(
+			s_pBuffer, s_pTextBitmap,
+			sOrigin.sUwCoord.uwX + pColOffs[i], sOrigin.sUwCoord.uwY + 16,
+			14, FONT_COOKIE
+		);
+	}
+
+	tPlan sPlan = {
+		.pMinerals = {
+			{TILE_SILVER_1, 10},
+			{TILE_GOLD_1, 5}
+		},
+		.ubMineralCount = 2,
+		.ulAlternativeSum = 100000
+	};
+
+	ubRowOffsY += g_pFont->uwHeight + 1;
+
+	blitRect(
+		s_pBuffer, sOrigin.sUwCoord.uwX + pColOffs[0], ubRowOffsY,
+		WINDOW_WIDTH - 2 * pColOffs[0], 1, 14
+	);
+
+	ubRowOffsY += 10 - g_pFont->uwHeight;
+
+	char szBfr[10];
+	for(UBYTE i = 0; i < sPlan.ubMineralCount; ++i) {
+		fontFillTextBitMap(
+			g_pFont, s_pTextBitmap, s_pMineralNames[sPlan.pMinerals[i].ubMineralType]
+		);
+		fontDrawTextBitMap(
+			s_pBuffer, s_pTextBitmap,
+			sOrigin.sUwCoord.uwX + pColOffs[0], ubRowOffsY, 14, FONT_COOKIE
+		);
+
+		sprintf(szBfr, "%hhu", sPlan.pMinerals[i].ubCount);
+		fontFillTextBitMap(g_pFont, s_pTextBitmap, szBfr);
+		fontDrawTextBitMap(
+			s_pBuffer, s_pTextBitmap,
+			sOrigin.sUwCoord.uwX + pColOffs[1], ubRowOffsY, 14, FONT_COOKIE
+		);
+
+		sprintf(szBfr, "%hhu", 0);
+		fontFillTextBitMap(g_pFont, s_pTextBitmap, szBfr);
+		fontDrawTextBitMap(
+			s_pBuffer, s_pTextBitmap,
+			sOrigin.sUwCoord.uwX + pColOffs[2], ubRowOffsY, 14, FONT_COOKIE
+		);
+		ubRowOffsY += 10;
+	}
+
+	fontFillTextBitMap(g_pFont, s_pTextBitmap, "Days remaining:");
+	fontDrawTextBitMap(
+		s_pBuffer, s_pTextBitmap,
+		sOrigin.sUwCoord.uwX + 150, sOrigin.sUwCoord.uwY + 160, 14, FONT_COOKIE
+	);
+	sprintf(szBfr, "%d", 0);
+	fontFillTextBitMap(g_pFont, s_pTextBitmap, szBfr);
+	fontDrawTextBitMap(
+		s_pBuffer, s_pTextBitmap,
+		sOrigin.sUwCoord.uwX + 240, sOrigin.sUwCoord.uwY + 160,
+		14, FONT_COOKIE | FONT_RIGHT
+	);
+
+}
 
 void vendorGsCreate(void) {
 	s_isShown = windowShow();
@@ -21,6 +116,12 @@ void vendorGsCreate(void) {
 
 	s_pBuffer = g_pMainBuffer->pScroll->pBack;
 
+	s_pTextBitmap = fontCreateTextBitMap(
+		WINDOW_WIDTH, g_pFont->uwHeight
+	);
+
+	vendorDrawPlan();
+
 	// Process managers once so that backbuffer becomes front buffer
 	// Single buffering from now!
 	viewProcessManagers(g_pMainBuffer->sCommon.pVPort->pView);
@@ -29,6 +130,10 @@ void vendorGsCreate(void) {
 }
 
 void vendorGsLoop(void) {
+	if(keyUse(KEY_ESCAPE)) {
+		gameClose();
+		return;
+	}
 	if(keyUse(KEY_RETURN) || keyUse(KEY_SPACE)) {
 		gamePopState();
 		return;
@@ -39,6 +144,7 @@ void vendorGsDestroy(void) {
 	if(!s_isShown) {
 		return;
 	}
+	fontDestroyTextBitMap(s_pTextBitmap);
 	viewProcessManagers(g_pMainBuffer->sCommon.pVPort->pView);
 	copProcessBlocks();
 	vPortWaitForEnd(g_pMainBuffer->sCommon.pVPort);
--- src/window.c
+++ src/window.c
@@ -4,61 +4,67 @@
 
 #include "window.h"
 #include "game.h"
-
-#define WINDOW_WIDTH (320-64)
-#define WINDOW_HEIGHT (200)
+#include <ace/utils/bmframe.h>
 
 tBitMap *s_pBgBuffer;
+tBitMap *s_pFrame;
 
 void windowInit(void) {
 	s_pBgBuffer = bitmapCreate(
 		WINDOW_WIDTH, WINDOW_HEIGHT,
 		g_pMainBuffer->sCommon.pVPort->ubBPP, BMF_INTERLEAVED
 	);
+	s_pFrame = bitmapCreateFromFile("data/shop_border.bm");
 }
 
 void windowDeinit(void) {
+	bitmapDestroy(s_pFrame);
 	bitmapDestroy(s_pBgBuffer);
 }
 
 UBYTE windowShow(void) {
-	UWORD uwScrollX = g_pMainBuffer->pCamera->uPos.sUwCoord.uwX;
-	UWORD uwScrollY = g_pMainBuffer->pCamera->uPos.sUwCoord.uwY;
-	UWORD uwBufferHeight = g_pMainBuffer->uwMarginedHeight;
-	UWORD uwFoldScrollY = uwScrollY & (uwBufferHeight-1);
-	UWORD uwWindowOffsY = (g_pMainBuffer->sCommon.pVPort->uwHeight - WINDOW_HEIGHT) / 2;
-	UWORD uwWindowOffsX = (g_pMainBuffer->sCommon.pVPort->uwWidth - WINDOW_WIDTH) / 2;
-	if(uwBufferHeight - uwFoldScrollY < uwWindowOffsY + WINDOW_HEIGHT) {
+	tUwCoordYX sOrigin = windowGetOrigin();
+	if(g_pMainBuffer->uwMarginedHeight - sOrigin.sUwCoord.uwX < WINDOW_HEIGHT) {
 		// Not positioned evenly
 		return 0;
 	}
 	// Store content beneath window
 	blitCopyAligned(
 		g_pMainBuffer->pScroll->pBack,
-		uwScrollX + uwWindowOffsX, uwFoldScrollY + uwWindowOffsY,
+		sOrigin.sUwCoord.uwX, sOrigin.sUwCoord.uwY,
 		s_pBgBuffer, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT
 	);
 
-	// Draw window
-	blitRect(
-		g_pMainBuffer->pScroll->pBack,
-		uwScrollX + uwWindowOffsX, uwFoldScrollY + uwWindowOffsY,
-		WINDOW_WIDTH, WINDOW_HEIGHT, 0
+	// Draw window background
+	bmFrameDraw(
+		s_pFrame, g_pMainBuffer->pScroll->pBack,
+		sOrigin.sUwCoord.uwX, sOrigin.sUwCoord.uwY,
+		WINDOW_WIDTH / 16, WINDOW_HEIGHT / 16, 16
 	);
 	return 1;
 }
 
 void windowHide(void) {
+	tUwCoordYX sOrigin = windowGetOrigin();
+	// Restore content beneath window
+	blitCopyAligned(
+		s_pBgBuffer, 0, 0, g_pMainBuffer->pScroll->pBack,
+		sOrigin.sUwCoord.uwX, sOrigin.sUwCoord.uwY,
+		WINDOW_WIDTH, WINDOW_HEIGHT
+	);
+}
+
+tUwCoordYX windowGetOrigin(void) {
 	UWORD uwScrollX = g_pMainBuffer->pCamera->uPos.sUwCoord.uwX;
 	UWORD uwScrollY = g_pMainBuffer->pCamera->uPos.sUwCoord.uwY;
 	UWORD uwBufferHeight = g_pMainBuffer->uwMarginedHeight;
 	UWORD uwFoldScrollY = uwScrollY & (uwBufferHeight-1);
 	UWORD uwWindowOffsY = (g_pMainBuffer->sCommon.pVPort->uwHeight - WINDOW_HEIGHT) / 2;
 	UWORD uwWindowOffsX = (g_pMainBuffer->sCommon.pVPort->uwWidth - WINDOW_WIDTH) / 2;
-	// Restore content beneath window
-	blitCopyAligned(
-		s_pBgBuffer, 0, 0, g_pMainBuffer->pScroll->pBack,
-		uwScrollX + uwWindowOffsX, uwFoldScrollY + uwWindowOffsY,
-		WINDOW_WIDTH, WINDOW_HEIGHT
-	);
+
+	tUwCoordYX sCoord;
+	sCoord.sUwCoord.uwX = uwScrollX + uwWindowOffsX;
+	sCoord.sUwCoord.uwY = uwFoldScrollY + uwWindowOffsY;
+
+	return sCoord;
 }
