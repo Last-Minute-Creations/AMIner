b31a757ce3f0fb01a3616b742cc8a523ac06b5c2
KaiN <kain@piwnica.ws>
2023-03-13 17:41:00 +0100

    disabled teleport damage for now

--- src/vehicle.c
+++ src/vehicle.c
@@ -1193,13 +1193,13 @@ uint8_t vehiclesAreClose(void) {
 static void vehicleOnTeleportInPeak(ULONG ulData) {
 	tVehicle *pVehicle = (tVehicle*)ulData;
 	pVehicle->ubVehicleState = VEHICLE_STATE_MOVING;
-	UWORD uwMaxHealth = inventoryGetPartDef(INVENTORY_PART_HULL)->uwMax;
-	if(randUwMax(&g_sRand, 100) <= 5) {
-		vehicleHullDamage(pVehicle, uwMaxHealth);
-	}
-	else if(randUwMax(&g_sRand, 100) <= 20) {
-		vehicleHullDamage(pVehicle, uwMaxHealth / 2);
-	}
+	// UWORD uwMaxHealth = inventoryGetPartDef(INVENTORY_PART_HULL)->uwMax;
+	// if(randUwMax(&g_sRand, 100) <= 5) {
+	// 	vehicleHullDamage(pVehicle, uwMaxHealth);
+	// }
+	// else if(randUwMax(&g_sRand, 100) <= 20) {
+	// 	vehicleHullDamage(pVehicle, uwMaxHealth / 2);
+	// }
 }
 
 static void vehicleOnTeleportOutPeak(ULONG ulData) {
