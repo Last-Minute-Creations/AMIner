1b0600c629238541f95b0c92195c235331c63c27
KaiN <kain@piwnica.ws>
2023-07-10 16:46:02 +0200

    added missing include guards

--- src/save.h
+++ src/save.h
@@ -2,8 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef SAVE_H
+#define SAVE_H
+
 #include <ace/utils/file.h>
 
 UBYTE saveReadHeader(tFile *pFile, const char *szHeader);
 
 void saveWriteHeader(tFile *pFile, const char *szHeader);
+
+#endif // SAVE_H
