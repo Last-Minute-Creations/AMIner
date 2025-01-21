ba9bc61a3cc69f8e949ee11c2973ca518415d4a2
KaiN <kain@piwnica.ws>
2025-01-21 19:43:31 +0100

    update teleporter flipbook to final gfx

--- src/flipbook.c
+++ src/flipbook.c
@@ -133,14 +133,14 @@ void flipbookManagerCreate(void) {
 
 	flipbookAnimDataCreate(
 		&s_pAnimDatas[FLIPBOOK_KIND_TELEPORTER_OUT], 20, 25,
-		(UBYTE[]){0, 1, 2, 3, 4, 5, 6, 7, 0, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
+		(UBYTE[]){0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24},
 		(UBYTE[]){4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 		s_pTeleporterFrames, s_pTeleporterFramesMask, &s_sBobRingWide, s_pSfxTeleport
 	);
 	flipbookAnimDataCreate(
-		&s_pAnimDatas[FLIPBOOK_KIND_TELEPORTER_IN], 8, 16,
-		(UBYTE[]){23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8},
-		(UBYTE[]){2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+		&s_pAnimDatas[FLIPBOOK_KIND_TELEPORTER_IN], 8, 17,
+		(UBYTE[]){24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8},
+		(UBYTE[]){2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 		s_pTeleporterFrames, s_pTeleporterFramesMask, &s_sBobRingWide, s_pSfxTeleport
 	);
 	flipbookAnimDataCreate(
