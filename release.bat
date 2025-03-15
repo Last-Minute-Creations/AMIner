f04887cd9c9e695f8cc24a8fbf16bac51ba3a68e
KaiN <kain@piwnica.ws>
2025-03-15 15:30:07 +0100

    remove release.bat

--- release.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir game
-copy aminer game
-copy aminer.info game
-xcopy data game\data /e /i /h
-mkdir game\s
-echo aminer > game\s\startup-sequence
-exe2adf aminer -l "AMIner" -a "aminer.adf" -0 -d game
-rmdir game /s /q
